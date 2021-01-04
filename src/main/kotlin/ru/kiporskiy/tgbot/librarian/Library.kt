daed94f4333cfa04a2ddff9445094448f200deeb
alexey_kip <kiporscky@live.ru>
2021-01-04 13:23:39 +0500

    Новый функции библиотеки

--- src/main/kotlin/ru/kiporskiy/tgbot/librarian/Library.kt
+++ src/main/kotlin/ru/kiporskiy/tgbot/librarian/Library.kt
@@ -1,5 +1,6 @@
 package ru.kiporskiy.tgbot.librarian
 
+import java.time.LocalDateTime
 import java.util.concurrent.ConcurrentSkipListMap
 
 /**
@@ -38,6 +39,21 @@ class Bookshelf {
         books[book.isbn] = book
     }
 
+    operator fun plusAssign(element: Book) {
+        addBook(element)
+    }
+
+    /**
+     * Удалить книгу, если она есть
+     */
+    fun removeBook(book: Book) {
+        books -= book.isbn
+    }
+
+    operator fun minusAssign(element: Book) {
+        removeBook(element)
+    }
+
     /**
      * Вернуть забронированную книгу на полку.
      * Вернуть можно только ту книгу, которая уже была добавлена, а потом забронирована
@@ -55,4 +71,133 @@ class Bookshelf {
     fun booking(book: Book) {
         books.remove(book.isbn) ?: throw IllegalStateException()
     }
+}
+
+/**
+ * Читательский билет
+ * Отображает какая книга у какого читателя "на руках".
+ */
+data class LibraryCard(
+    val book: Book,
+    val reader: Reader,
+    val startDate: LocalDateTime,
+    val recommendFinishDate: LocalDateTime
+)
+
+/**
+ * Библиотека, объединяющая читателей и книги
+ */
+class Library {
+
+    /**
+     * Все читатели, зарегистрированные в системе
+     */
+    private val readers: MutableList<Reader> = ArrayList()
+
+    /**
+     * Все книги, зарегистрированные в системе
+     */
+    private val books: MutableList<Book> = ArrayList()
+
+    /**
+     * Все категории книг
+     */
+    private val categories: MutableList<BookCategory> = ArrayList()
+
+    /**
+     * Книги, доступные для чтения
+     */
+    private val bookshelf = Bookshelf()
+
+    /**
+     * Занятые книги
+     */
+    private val cards: List<LibraryCard> = ArrayList()
+
+    /**
+     * Добавить книгу в библиотеку
+     */
+    fun addBook(book: Book) {
+        books += book
+        bookshelf += book
+    }
+
+    /**
+     * Удалить книгу из библиотеки
+     */
+    fun removeBook(book: Book) {
+        books -= book
+        bookshelf -= book
+    }
+
+    /**
+     * Добавить новую категорию. "Родители" добавятся автоматически, если не были добавлены раньше
+     */
+    tailrec fun addCategoryBook(category: BookCategory) {
+        if (category != BookCategory.getUnclassifiedCategory()) {
+            categories += category
+            addCategoryBook(category)
+        }
+    }
+
+    /**
+     * Удалить категорию книг вместе с дочерними категориями
+     * Если у категории или ее потомков есть книги, то удалить ее будет нельзя
+     *
+     * @throws IllegalArgumentException при невозможности удалить категорию, у которой еще остались книги
+     */
+    fun removeCategoryBook(category: BookCategory) {
+        val hasBooksWithCategory = books.any { category.isParentFor(it.category) }
+
+        if (hasBooksWithCategory)
+            throw IllegalArgumentException("Категорию нельзя удалить, потому что еще есть книги, которые в нее входят")
+        else
+            categories.removeIf { category.isParentFor(it) }
+    }
+
+    /**
+     * Добавить книгу в библиотеку
+     */
+    fun addReader(reader: Reader) {
+        readers += reader
+    }
+
+    /**
+     * Удалить книгу из библиотеки
+     */
+    fun removeReader(reader: Reader) {
+        readers -= reader
+    }
+
+    /**
+     * Получить все доступные книги для чтения
+     */
+    fun getAllBooksForReadings() = this.bookshelf.getBooks()
+
+    /**
+     * Постраничное получение всех доступных книг
+     */
+    fun getAllBooksForReadings(page: Int, pageSize: Int) = getAllBooksForReadingsPageable(page, pageSize)
+
+    fun getAllBooksForReadingsPageable(page: Int, pageSize: Int, category: BookCategory? = null): List<Book> {
+        val allBooks = category?.let { getAllBooksForReadings(it) } ?: getAllBooksForReadings()
+        val startIndex = page * pageSize
+        val finishIndex = startIndex + pageSize + 1
+
+        if (allBooks.size <= startIndex)
+            return listOf()
+
+        return allBooks.subList(startIndex, finishIndex)
+    }
+
+    /**
+     * Получить все доступные для чтения книги определенной категории
+     */
+    fun getAllBooksForReadings(category: BookCategory) = this.bookshelf.getBooks(category)
+
+    /**
+     * Получить все доступные для чтения книги определенной категории
+     */
+    fun getAllBooksForReadings(category: BookCategory, page: Int, pageSize: Int) =
+        getAllBooksForReadingsPageable(page, pageSize, category)
 }
