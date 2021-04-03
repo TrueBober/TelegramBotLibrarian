cc6ff484368fbc36c12e492748bff03e603ec35f
alexey_kip <kiporscky@live.ru>
2021-04-03 21:30:27 +0500

    [new] контекст

---
+++ src/main/kotlin/ru/kiporskiy/tgbot/librarian/simple/core/Context.kt
@@ -0,0 +1,7 @@
+package ru.kiporskiy.tgbot.librarian.simple.core
+
+/**
+ * Интерфейс контекста
+ * Контекст нужен для "запоминания" того, что пользователь делал на предыдущем шаге.
+ */
+interface Context
