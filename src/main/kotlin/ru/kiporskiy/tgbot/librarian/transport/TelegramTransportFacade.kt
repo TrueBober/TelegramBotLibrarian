0a18099f82ea7feff01b65a9b692b49a6004c536
Кипорский <kiporskiy_ay@technology45.ru>
2021-09-23 13:12:26 +0500

    [add]: добавление слушателей команд получаемых от телеграма

--- src/main/kotlin/ru/kiporskiy/tgbot/librarian/transport/TelegramTransportFacade.kt
+++ src/main/kotlin/ru/kiporskiy/tgbot/librarian/transport/TelegramTransportFacade.kt
@@ -1,12 +1,82 @@
 package ru.kiporskiy.tgbot.librarian.transport
 
 import com.pengrad.telegrambot.TelegramBot
+import com.pengrad.telegrambot.model.Message
+import com.pengrad.telegrambot.model.MessageEntity
+import com.pengrad.telegrambot.model.Update
 import com.pengrad.telegrambot.request.SendMessage
 
 /**
  * Класс для взаимодействия с ботапи телеграма.
  */
 class TelegramTransportFacade(private val bot: TelegramBot) {
+
+    /**
+     * Слушатели для команд, поступающих от бота
+     */
+    private val commandsListener: MutableList<(TelegramCommand) -> Unit> = mutableListOf()
+
+    init {
+        this.runTelegramUpdatesListener()
+    }
+
+    /**
+     * Запуск слушаетеля событий из телеграма
+     */
+    private fun runTelegramUpdatesListener() {
+        this.bot.setUpdatesListener { updatesListener(it) }
+    }
+
+    /**
+     * Обработка полученной порции событий
+     */
+    private fun updatesListener(updates: List<Update>): Int {
+        updates
+            .mapNotNull { upd -> upd.message() }
+            .forEach { processMessage(it) }
+        return updates.last().updateId()
+    }
+
+    /**
+     * Обработка сообщения
+     */
+    private fun processMessage(message: Message) {
+        when {
+            startWithCommand(message) -> {
+                val command = getCommand(message)
+                commandsListener.forEach { it.invoke(command) }
+            }
+        }
+    }
+
+    /**
+     * Проверить, что сообщение начинается с команды боту
+     */
+    private fun startWithCommand(message: Message): Boolean {
+        if (message.text() == null || message.text().isBlank()) {
+            return false
+        }
+        val entities = message.entities() ?: return false
+        val firstEntity = entities.firstOrNull() ?: return false
+        return firstEntity.type() == MessageEntity.Type.bot_command
+    }
+
+    /**
+     * Получить команду с аргументами
+     */
+    private fun getCommand(message: Message): TelegramCommand {
+        val commandPosition = message.entities().first()
+        val commandPositionStart = commandPosition.offset()
+        val commandPositionEnd = commandPosition.offset() + commandPosition.length() + 1
+        val command = message.text().substring(commandPositionStart, commandPositionEnd)
+
+        if (message.text().length == command.length) {
+            return TelegramCommand(command, listOf())
+        } else {
+            val arguments = message.text().substring(commandPositionEnd).split(" ")
+            return TelegramCommand(command, arguments)
+        }
+    }
 
     /**
      * Отправка простого текстового сообщения
@@ -16,6 +86,12 @@ class TelegramTransportFacade(private val bot: TelegramBot) {
         bot.execute(request)
     }
 
+    /**
+     * Добавить слушателя для команд, получаемых из телеграма
+     */
+    fun addOnTextMessageListener(listener: (TelegramCommand) -> Unit) {
+        this.commandsListener += listener
+    }
 }
 
 /**
@@ -47,3 +123,8 @@ data class UsernameTelegramChatId(override val id: String) : TelegramChatId
  * Идентификатор, ассоциированный с числовым идентификатором пира
  */
 data class NumberTelegramChatId(override val id: Int) : TelegramChatId
+
+/**
+ * Команда, полученная из телеграма
+ */
+data class TelegramCommand(val title: String, val args: List<String>)
